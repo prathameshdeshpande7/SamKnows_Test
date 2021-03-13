b37ae49d8e0294d79dbedb6db6208c4f21d787f0
Prathamesh Deshpande <prathameshdeshpande7@gmail.com>
2021-03-13 14:59:49 +0000

    Added build.sh and moved compilation commands from the run.sh script
    Keep a separate build bash script which will clean, make all and
    make install the application. run.sh will only be used for testing
    and firing HTTP requests.
    
    Increase the number of default requests to fire for testing.

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-make clean
-make all
-sudo make install
-./mycurl_test -H "Accept: application/json" -H "Content-Type: application/json" -X GET -n 11 --header "Accept: application/xml" --url http://www.google.com/
+./mycurl_test -H "Accept: application/json" -H "Content-Type: application/json" -X GET -n 500 --header "Accept: application/xml" --url http://www.google.com/
