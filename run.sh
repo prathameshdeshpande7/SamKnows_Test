9830d8f196140ab495b17e8967bd6e2c504ff57d
Prathamesh Deshpande <prathameshdeshpande7@gmail.com>
2021-03-13 20:34:26 +0000

    Added configuration parameters to the run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-./mycurl_test -H "Accept: application/json" -H "Content-Type: application/json" -X GET -n 500 --header "Accept: application/xml" --url http://www.google.com/
+NUM=10
+H1='Content-Type: application/json'
+H2='Accept: application/json'
+H3='Accept-Charset: utf-8'
+URL='http://www.google.com'
+TYPE='GET'
+
+echo "Start of test"
+
+for i in `seq 1 10`;
+do
+	echo ----------------------------------------------------------------------------------
+	echo ITERATION $i
+	TEST="$(./mycurl_test -X "$TYPE" -H "${H1}" -H "${H2}" -H "${H3}" -n $NUM --url "$URL")"
+	echo ----------------------------------------------------------------------------------
+	echo
+done
+
+echo "All done"
